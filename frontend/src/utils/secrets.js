dad728516ccc78f177e3f61b289bdb7e1aae84cf
Yash Wairagade <theyashwairagade@gmail.com>
2024-06-03 03:13:24 +0530

    Add development config files

---
+++ frontend/src/utils/secrets.js
@@ -0,0 +1,22 @@
+// Get your api key from https://developer.themoviedb.org/docs/getting-started
+export const API_KEY= "Enter your api key here";
+
+export const API_BASE_URL= "https://api.themoviedb.org/3";
+export const IMAGE_URL="https://image.tmdb.org/t/p/w500";
+
+// Change only if you are hosting the website
+export const BACKEND_URL="http://localhost:5000";
+
+// login at https://firebase.google.com/
+// Move to console
+// Register project
+// Enter config below
+export const FIREBASE_CONFIG={
+    apiKey: "Enter your API key",
+    authDomain: "Enter your auth domain",
+    projectId: "Enter your project id",
+    storageBucket: "Enter your storage bucket",
+    messagingSenderId: "Enter your messaging sender id",
+    appId: "Enter your app id",
+    measurementId: "Enter your measurement id"
+  };
